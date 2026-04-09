34929c06d2d1c3fbdeaff43ba747039b2708ff30
DefNotTeto <sasinewashere@gmail.com>
2026-04-09 05:48:38 -0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,15 +1,32 @@
+-------------------------------------------- // MAIN EXAMPLE // ---------------------------------------------------------
+
 -- Controls:
 --  R: Toggle Gui
 --  WASD: Move Cursor
 --  V: Click
 
+-- // Services
+local cloneref = (cloneref or clonereference) or function(obj)
+    return obj
+end
+local starterGui = game:GetService('StarterGui')
+
+-- // Main Library
 local Haze = loadstring(game:HttpGet('https://github.com/DefNotTeto/Haze-Library/raw/refs/heads/main/Library.lua'))()
 
+-- // Window
 local Window = Haze:CreateWindow({
     Name = 'teto',
     Color = {200, 100, 120},
     Position = {500, 250}
 })
 
-local tab1 = Window:CreateTab({Name = 'Tab 1'})
-local tab2 = Window:CreateTab({Name = 'Tab 2'})
+-- // Tabs
+local tab1 = Window:CreateTab({
+    Name = 'Teto 1'
+})
+local tab2 = Window:CreateTab({
+    Name = 'Teto 2'
+})
+
+----------------------------------------------------------------------------------------------------------------------
